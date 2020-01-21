01321e08acc51aa83a4ff80d64982021cc6c9d95
Henry Cui <z.henry.cui@gmail.com>
2020-01-21 16:38:40 +0900

    corrected logic

--- app/controllers/converter_controller.rb
+++ app/controllers/converter_controller.rb
@@ -10,23 +10,23 @@ class ConverterController < ApplicationController
     else
       file_name = "#{Rack::Utils.escape(url)}.html"
       file_path = Rails.root.join('tmp', file_name).to_s
-      if not @caches.include? file_name
-        require 'selenium-webdriver'
 
-        if Rails.env.production?
-          Selenium::WebDriver::Chrome.path = ENV["GOOGLE_CHROME_SHIM"]
-        end
-        options = Selenium::WebDriver::Chrome::Options.new
-        options.add_argument('headless')
-        options.add_argument('disable-gpu')
-        options.add_argument("--disable-dev-shm-usage")
-        options.add_argument("--no-sandbox")
-        driver = Selenium::WebDriver.for :chrome, options: options
+      require 'selenium-webdriver'
 
-        driver.get url
-        File.write(file_path, driver.page_source)
-        driver.quit
+      if Rails.env.production?
+        Selenium::WebDriver::Chrome.path = ENV["GOOGLE_CHROME_SHIM"]
       end
+      options = Selenium::WebDriver::Chrome::Options.new
+      options.add_argument('headless')
+      options.add_argument('disable-gpu')
+      options.add_argument("--disable-dev-shm-usage")
+      options.add_argument("--no-sandbox")
+      driver = Selenium::WebDriver.for :chrome, options: options
+
+      driver.get url
+      File.write(file_path, driver.page_source)
+      driver.quit
+
       render html: File.read(file_path).html_safe
     end
   end
