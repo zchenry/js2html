7c3c14f9d5a986b79fba31acb300963f80d14a63
Henry Cui <z.henry.cui@gmail.com>
2020-01-21 16:13:28 +0900

    adjust for heroku

--- app/controllers/converter_controller.rb
+++ app/controllers/converter_controller.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 class ConverterController < ApplicationController
   def convert
     tmp_dir = Rails.root.join('tmp').to_s
@@ -11,9 +12,17 @@ class ConverterController < ApplicationController
       file_path = Rails.root.join('tmp', file_name).to_s
       if not @caches.include? file_name
         require 'selenium-webdriver'
+
+        if Rails.env.production?
+          Selenium::WebDriver::Chrome.path = ENV["GOOGLE_CHROME_SHIM"]
+        end
         options = Selenium::WebDriver::Chrome::Options.new
-        options.add_argument '--headless'
+        options.add_argument('headless')
+        options.add_argument('disable-gpu')
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--no-sandbox")
         driver = Selenium::WebDriver.for :chrome, options: options
+
         driver.get url
         File.write(file_path, driver.page_source)
         driver.quit
