227fc849da43e8573dd15b46fbfd333a07014dd9
Henry Cui <z.henry.cui@gmail.com>
2020-01-21 10:55:08 +0900

    development done

--- Gemfile
+++ Gemfile
@@ -23,7 +23,5 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
 
-gem 'capybara', '>= 2.15'
 gem 'selenium-webdriver'
-gem 'capybara-webkit'
 gem 'webdrivers'
--- app/controllers/converter_controller.rb
+++ app/controllers/converter_controller.rb
@@ -1,18 +1,24 @@
 class ConverterController < ApplicationController
   def convert
     tmp_dir = Rails.root.join('tmp').to_s
-    @caches = Dir["#{tmp_dir}/*.html"]
-
+    @caches = Dir["#{tmp_dir}/*.html"].map{ |f| File.basename f }
     url = params[:url]
+
     if url.nil?
       render :convert
     else
       file_name = "#{Rack::Utils.escape(url)}.html"
-      if @caches.include? file_name
-        render html: File.read(Rails.root.join('tmp', file_name).to_s).html_safe
-      else
-        render :nil
+      file_path = Rails.root.join('tmp', file_name).to_s
+      if not @caches.include? file_name
+        require 'selenium-webdriver'
+        options = Selenium::WebDriver::Chrome::Options.new
+        options.add_argument '--headless'
+        driver = Selenium::WebDriver.for :chrome, options: options
+        driver.get url
+        File.write(file_path, driver.page_source)
+        driver.quit
       end
+      render html: File.read(file_path).html_safe
     end
   end
 end
